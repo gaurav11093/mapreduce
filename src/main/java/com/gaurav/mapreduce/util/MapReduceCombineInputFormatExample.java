1b89fabb8a37fc1b5687ea10ae7e8f3fcb5cf9d8
Gaurav Garg <gaurav.garg@mobileum.com>
2018-03-08 12:51:45 +0530

    Support to add external jars

--- src/main/java/com/gaurav/mapreduce/util/MapReduceCombineInputFormatExample.java
+++ src/main/java/com/gaurav/mapreduce/util/MapReduceCombineInputFormatExample.java
@@ -1,10 +1,13 @@
 package com.gaurav.mapreduce.util;
 
 import com.gaurav.mapreduce.map.MapCombineInputFormat;
+import com.mobileum.common.hdfs.HdfsUtil;
 import com.mobileum.roamingmodel.input.LsltrBasedOutRoamerMessageInputFormat;
+import com.mobileum.roamingmodel.input.OutRoamerMessageInputFormat;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -24,14 +27,23 @@ public class MapReduceCombineInputFormatExample extends Configured implements or
     Job job = Job.getInstance(conf, "PriorityInputFormat");
     job.setJarByClass(MapReduceCombineInputFormatExample.class);
     job.setMapperClass(MapCombineInputFormat.class);
-    job.setInputFormatClass(LsltrBasedOutRoamerMessageInputFormat.class);
-    // job.getConfiguration().set("spark.lastProcessedTime", "1481763600");
-    job.getConfiguration().set("spark.roamingmodel.datasource.priority", "[NRTRDE_IN,TAP_IN]");
+    for (FileStatus fs : HdfsUtil.get().getAllPathsRecursively(new Path[] { new Path("/lib/WS") })) {
+      job.addFileToClassPath(fs.getPath());
+    }
+    // job.addArchiveToClassPath(new Path("/lib/WS/*"));
+    // job.addArchiveToClassPath(new Path("/lib/WS/datamodelcommon-1.0-SNAPSHOT.jar"));
+    if (args[4].equals("lsltr")) {
+      job.setInputFormatClass(LsltrBasedOutRoamerMessageInputFormat.class);
+    } else {
+      job.setInputFormatClass(OutRoamerMessageInputFormat.class);
+    }
+    job.getConfiguration().set("roamingmodel.lastProcessedTime", args[0]); // "1481763600"
+    job.getConfiguration().set("roamingmodel.datasource.priority", args[1]); // "[NRTRDE_IN,TAP_IN]"
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
     // job.setNumReduceTasks(3);
-    FileInputFormat.addInputPath(job, new Path("output/roaming_model/outroamer/message/calltype=international/bintime=-1/part*"));
-    job.getConfiguration().set("mapreduce.output.fileoutputformat.outputdir", "gg/priority_output");
+    FileInputFormat.addInputPath(job, new Path(args[2])); // "output/roaming_model/outroamer/message/calltype=international/bintime=1/part*"
+    job.getConfiguration().set("mapreduce.output.fileoutputformat.outputdir", args[3]); // "gg/priority_output"
     // FileOutputFormat.setOutputPath(new JobConf(conf), new Path("gg/mapreduce/output"));
     return job.waitForCompletion(true) ? 0 : 1;
 
